17021d870616b730bca9a77654b0c68106d47aff
VictorCano18 <vcano@entersolteam.com.mx>
2024-06-06 16:25:35 -0600

    feat(master): add responsiveness, change color and font size

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -10,7 +10,9 @@ const Country = () => {
     const currencies = Object.values(country.currencies)
     const allCurrencies = currencies.map(curr => curr.name)
     const neighbors = data.filter(current => country.borders.includes(current.cioc))
-    console.log(neighbors)
+    const numberWithCommas = (num) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     return (
         <div>
@@ -22,12 +24,12 @@ const Country = () => {
                 <div><img src={country.flags.svg} alt={country.flags.alt} className="flagImageCountry" /></div>
                 <div className="tableCountry">
                     <div className="nameContainerCountry">
-                        <div>{country.name.common}</div>
+                        <div><strong>{country.name.common}</strong></div>
                         <div>{country.name.official}</div>
                     </div>
                     <div className="populationAreaContainerCountry">
-                        <div><p>Population</p> {country.population}</div>
-                        <div><p>Area (km²)</p> {country.area}</div>
+                        <div><p>Population</p> {numberWithCommas(country.population)}</div>
+                        <div><p>Area (km²)</p> {numberWithCommas(country.area)}</div>
                     </div>
                     <div className="listInfoCountry">
                         <div>Capital <p>{country.capital.length > 1 ? country.capital.join(', ') : country.capital[0]}</p></div>
@@ -35,7 +37,7 @@ const Country = () => {
                         <div>Language <p>{languages.length > 1 ? languages.join(', ') : languages[0]}</p></div>
                         <div>Currencies <p>{allCurrencies.length > 1 ? allCurrencies.join(', ') : allCurrencies[0]}</p></div>
                         <div className="lastListInfoCountry">Continents <p>{country.continents.length > 1 ? country.continents.join(', ') : country.continents[0]}</p></div>
-                        <div>Neighbouring Countries</div>
+                        <div className="neighbouringCountriesTitle">Neighbouring Countries</div>
                         <div className="neighborsContainer">
                             {neighbors.length ? neighbors.map(country => (
                                 <Link to='/selected-country' state={{ country, data }}>
